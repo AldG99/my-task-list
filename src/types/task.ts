2bebbcad22727d0ba878e3a27fd6ce168eb9b88a
AldG99 <a.garciaglz17@gmail.com>
2024-11-30 14:59:46 -0600

    Definir la interfaz de tareas para modelar las tareas de la aplicación

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,5 +1,11 @@
+// Define la interfaz `Task`, que representa la estructura de una tarea en la aplicación.
 export interface Task {
   id: number;
+  // Identificador único para cada tarea. Es un número (por ejemplo, generado con `Date.now()`).
+
   text: string;
+  // El texto descriptivo de la tarea, que indica qué debe hacerse. Es una cadena de texto.
+
   completed: boolean;
+  // Indica si la tarea está completada o no. Es un valor booleano (`true` o `false`).
 }
