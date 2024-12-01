3e45fc3ada0d76bbce72f85beca89ac4edc90708
AldG99 <a.garciaglz17@gmail.com>
2024-12-01 13:05:39 -0600

    Configuración del componente principal con AllScreen como punto de entrada

--- App.tsx
+++ App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+// Importa React para usar componentes funcionales.
+
 import { AllScreen } from './src/screens/allScreen';
+// Importa el componente `AllScreen` desde su ruta correspondiente.
+// `AllScreen` es la pantalla principal de la aplicación, que muestra la lista de tareas.
 
 export default function App() {
+  // Define el componente principal de la aplicación.
+
   return <AllScreen />;
+  // Retorna el componente `AllScreen` como la interfaz principal que se renderiza.
+  // Este será el punto de entrada de la aplicación.
 }
